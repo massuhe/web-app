e17ec6b4a45ba1f6b612ee105bdbddf4ec292415
Esteban Massuh <massuhe@outlook.com>
2017-08-01 19:02:44 -0300

    Se agruparon los componentes del navbar en una misma carpeta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NavbarItemComponent } from './navbar-item/navbar-item.component';
-import { NavbarItemDropdownComponent } from './navbar-item-dropdown/navbar-item-dropdown.component';
-import { ItemDropdownComponent } from './item-dropdown/item-dropdown.component';
+import { NavbarComponent } from './nav/navbar/navbar.component';
+import { NavbarItemComponent } from './nav/navbar-item/navbar-item.component';
+import { NavbarItemDropdownComponent } from './nav/navbar-item-dropdown/navbar-item-dropdown.component';
+import { ItemDropdownComponent } from './nav/item-dropdown/item-dropdown.component';
 
 @NgModule({
   declarations: [
--- src/app/nav/navbar-item-dropdown/navbar-item-dropdown.component.ts
+++ src/app/nav/navbar-item-dropdown/navbar-item-dropdown.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input } from '@angular/core';
   selector: 'app-navbar-item-dropdown',
   templateUrl: './navbar-item-dropdown.component.html',
   styleUrls: ['./navbar-item-dropdown.component.scss',
-              '../../assets/scss/layout/_navbar.scss'
+              '../../../assets/scss/layout/_navbar.scss'
   ]
 })
 export class NavbarItemDropdownComponent implements OnInit {
